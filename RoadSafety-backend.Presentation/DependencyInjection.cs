4bd957e2ef0fa2e210be9ea1eb3ed68ff60430da
kxmf <kxm6122@gmail.com>
2026-05-07 15:03:12 +0500

    feat: fix scalar

--- RoadSafety-backend.Presentation/DependencyInjection.cs
+++ RoadSafety-backend.Presentation/DependencyInjection.cs
@@ -1,11 +1,25 @@
+using Microsoft.AspNetCore.Http.Json;
+using System.Text.Json.Serialization;
+
 namespace RoadSafety_backend.Presentation;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddPresentation(this IServiceCollection services)
     {
-        services.AddControllers();
+
+        services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            });
+
+        services.Configure<JsonOptions>(options =>
+        {
+            options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        });
+
         services.AddOpenApi();
         return services;
     }
-}
+} 
