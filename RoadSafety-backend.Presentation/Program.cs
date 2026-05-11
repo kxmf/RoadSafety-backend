9cf6c2a01c166f1d56117d09f3b526d510c07d08
kxmf <kxm6122@gmail.com>
2026-05-11 19:26:27 +0500

    add program.cs migrations and fixed docker-compose.yml+.env.example

--- RoadSafety-backend.Presentation/Program.cs
+++ RoadSafety-backend.Presentation/Program.cs
@@ -2,6 +2,8 @@ using RoadSafety_backend.Presentation;
 using RoadSafety_backend.Application;
 using RoadSafety_backend.Infrastructure;
 using Scalar.AspNetCore;
+using RoadSafety_backend.Infrastructure.Persistence.PostgreSQL.Context;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,16 @@ builder.Services.AddApplication()
                 .AddPresentation();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        await dbContext.Database.MigrateAsync();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
