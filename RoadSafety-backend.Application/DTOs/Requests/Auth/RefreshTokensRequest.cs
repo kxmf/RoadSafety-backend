d55876012092dc445d2fbc931c38997a40d3c3e6
ilya034 <68101303+ilya034@users.noreply.github.com>
2026-05-11 20:00:51 +0500

    Add validation for DTOs

--- RoadSafety-backend.Application/DTOs/Requests/Auth/RefreshTokensRequest.cs
+++ RoadSafety-backend.Application/DTOs/Requests/Auth/RefreshTokensRequest.cs
@@ -1,6 +1,9 @@
-﻿namespace RoadSafety_backend.Application.DTOs.Requests.Auth;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoadSafety_backend.Application.DTOs.Requests.Auth;
 
 public record RefreshTokensRequest
 {
+    [Required(ErrorMessage = "Refresh token is required")]
     public required string RefreshToken { get; init; }
 }
